78c9990b661a487349c6b63538f3c29ecfb0de91
migueedlsantos97 <63765970+migueedlsantos97@users.noreply.github.com>
2020-09-05 20:38:24 -0300

    .

--- Workspace_initial/js/product-info.js
+++ Workspace_initial/js/product-info.js
@@ -1,6 +1,81 @@
+var product = {};
+
+function showImagesGallery(array) {
+  let htmlContentToAppend = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let imageSrc = array[i];
+
+    htmlContentToAppend +=
+      `
+            <li data-thumb="` +
+      imageSrc +
+      `">
+                <a href="` +
+      imageSrc +
+      `" data-fancybox="gallery">
+                    <img class="img-prod-info" src="` +
+      imageSrc +
+      `" />
+                </a>
+            </li>
+        `;
+
+    document.getElementById(
+      "productImagesGallery"
+    ).innerHTML = htmlContentToAppend;
+  }
+}
+
+function showRelatedProducts(array) {
+  let htmlContentToAppend = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let relatedProducts = array[i];
+
+    htmlContentToAppend +=
+      `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 w-100">
+                <a href="product-info.html"> <img class="img-fluid img-thumbnail" src="img/car` +
+      relatedProducts +
+      `.jpg" alt=""></a>
+            </div>
+        </div>
+        `;
+
+    document.getElementById(
+      "productRelatedProducts"
+    ).innerHTML = htmlContentToAppend;
+  }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      product = resultObj.data;
 
+      let productNameHTML = document.getElementById("productName");
+      let productDescriptionHTML = document.getElementById(
+        "productDescription"
+      );
+      let productCurrencyAndCostHTML = document.getElementById(
+        "productCurrency"
+      );
+      let productSoldCountHTML = document.getElementById("productSoldCount");
+
+      productNameHTML.innerHTML = product.name;
+      productDescriptionHTML.innerHTML = product.description;
+      productCurrencyAndCostHTML.innerHTML =
+        product.currency + ` ` + product.cost;
+      productSoldCountHTML.innerHTML = product.soldCount;
+
+      //Muestro las imagenes en forma de galería
+      showImagesGallery(product.images);
+      //Muestro imagenes de los productos relacionados
+      showRelatedProducts(product.relatedProducts);
+    }
+  });
 });
--- Workspace_initial/js/products.js
+++ Workspace_initial/js/products.js
@@ -172,46 +172,12 @@ document.addEventListener("DOMContentLoaded", function (e) {
   document.getElementById("b-search").addEventListener("click", function () {
     busqueda = document.getElementById("p-search").value;
 
-    if (
+    /*if (
       busqueda == "Susuki Celerio" ||
       "Chevrolet Onix Joy" ||
       "Fiat Way" ||
       "Peugeot 208"
     ) {
-    }
+    }*/
   });
 });
-
-/*<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
-
-<ul id="myUL">
-  <li><a href="#">Adele</a></li>
-  <li><a href="#">Agnes</a></li>
-
-  <li><a href="#">Billy</a></li>
-  <li><a href="#">Bob</a></li>
-
-  <li><a href="#">Calvin</a></li>
-  <li><a href="#">Christina</a></li>
-  <li><a href="#">Cindy</a></li>
-</ul>*/
-
-/*function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}*/
