9bf6b30a0bbcfe2e406158c59371b896539b1a59
migueedlsantos97 <63765970+migueedlsantos97@users.noreply.github.com>
2020-09-11 01:27:38 -0300

    Agregué formulario para nuevo comentario

--- Workspace_initial/js/product-info.js
+++ Workspace_initial/js/product-info.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         product.currency + ` ` + product.cost;
       productSoldCountHTML.innerHTML = product.soldCount;
 
-      //Muestro las imagenes en forma de galería
+      //Muestro las imagenes en forma de galería (sin controles)
       showImagesGallery(product.images);
       //Muestro imagenes de los productos relacionados
       showRelatedProducts(product.relatedProducts);
@@ -29,29 +29,92 @@ document.addEventListener("DOMContentLoaded", function (e) {
   });
 });
 
-//Agregué la función para mostrar los comentarios
+//Funcion que crea el rating de "caritas" para la calificación
+//rating = 0; Face:"meh-blank";
+//rating = 1; Face:"angry";Text:"Muy malo"
+//rating = 2; Face:"frown";Text:"Malo"
+//rating = 3; Face:"meh";Text:"Aceptable"
+//rating = 4; Face:"grin-alt";Text:"Muy bueno"
+//rating = 5; Face:"grin-stars";Text:"Excelente"
+function ratingFaces(rating) {
+  let faces;
+  if (rating === 0) {
+    faces = '<span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span>'
+  } else if (rating === 1) {
+    faces = '<span id="faces"><i class="far fa-angry"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span>'
+  } else if (rating === 2) {
+    faces = '<span id="faces"><i class="far fa-frown"></i></span><span id="faces"><i class="far fa-frown"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span>'
+  } else if (rating === 3) {
+    faces = '<span id="faces"><i class="far fa-meh"></i></span><span id="faces"><i class="far fa-meh"></i></span><span id="faces"><i class="far fa-meh"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span>'
+  } else if (rating === 4) {
+    faces = '<span id="faces"><i class="far fa-grin-alt"></i></span><span id="faces"><i class="far fa-grin-alt"></i></span><span id="faces"><i class="far fa-grin-alt"></i></span><span id="faces"><i class="far fa-grin-alt"></i></span><span id="faces"><i class="far fa-meh-blank"></i></span>'
+  } else {
+    faces = '<span id="faces"><i class="far fa-grin-stars"></i></span><span id="faces"><i class="far fa-grin-stars"></i></span><span id="faces"><i class="far fa-grin-stars"></i></span><span id="faces"><i class="far fa-grin-stars"></i></span><span id="faces"><i class="far fa-grin-stars"></i></span>'
+  }
+  return faces;
+}
+
+let showFaces;
+//Función para mostrar los comentarios
 function showComments(array) {
+  let htmlContentToAppend = "";
+
   for (let i = 0; i < array.length; i++) {
     let comment = array[i];
-    let score = "";
-    for (let i = 1; i < comment.score; i++) {
-      score += `<span class="fa fa.star" id="starUnchecked"></span>`
-    }
+    let showFaces = comment.score;
     htmlContentToAppend += `
-      <hr class ="my-3">
-      <div>
-      <span> `+ score + `</span>
-      <p> `+ comment.description + `</p>
-      <p> `+ `Publicado el ` + comment.dataTime + `</p>
-      </div>
-      `
-
+            <div class="card mb-2">
+                <div class="d-block">
+                    <div class="card-header align-user w-100">
+                    <div class="col m-0 pl-0" id="user-comment">`+ comment.user + '</div><div class="col text-right"> Calificación: ' + ratingFaces(showFaces) + `</div>
+                    </div>
+                </div>
+                <div class="card-body ">
+                    <blockquote class="blockquote mb-0">
+                    <div class="blockquote-footer text-right m-0">`+ comment.dateTime + `</div>
+                    <p class="p-comment">"`+ comment.description + `"</p>
+                    </blockquote>
+                </div>
+            </div>
+            `
     document.getElementById("comentarios").innerHTML = htmlContentToAppend;
   }
 }
+//Cargo los comentarios predeterminados en un JSON
 getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
   if (resultObj.status === "ok") {
     commentArray = resultObj.data;
   }
   showComments(commentArray);
 })
+//display propierty (display:none) ver esto para ocultar la pestaña activa y mostrar la otra
+
+//arreglo.push(nuevoElemento);
+
+
+//Función para mostrar las preguntas
+//Crear array estilo JSON para las preguntas
+/*function showComments(array) {
+  let htmlContentToAppend = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let comment = array[i];
+    let showFaces = comment.score;
+    htmlContentToAppend += `
+            <div class="card mb-2">
+                <div class="d-block">
+                    <div class="card-header align-user w-100">
+                    <div class="col m-0 pl-0" id="user-question">`+ comment.user + '</div><div class="col text-right"> Calificación: ' + ratingFaces(showFaces) + `</div>
+                    </div>
+                </div>
+                <div class="card-body ">
+                    <blockquote class="blockquote mb-0">
+                    <div class="blockquote-footer text-right m-0">`+ comment.dateTime + `</div>
+                    <p class="p-comment">"`+ comment.description + `"</p>
+                    </blockquote>
+                </div>
+            </div>
+            `
+    document.getElementById("comentarios").innerHTML = htmlContentToAppend;
+  }
+}*/
