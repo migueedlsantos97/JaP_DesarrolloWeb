e9492019320f4a553f85555378ffc634c56a736a
migueedlsantos97 <63765970+migueedlsantos97@users.noreply.github.com>
2020-10-28 00:35:34 -0300

    Mi perfil maquetado

--- Workspace_initial/js/my-profile.js
+++ Workspace_initial/js/my-profile.js
@@ -2,5 +2,20 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    var celular = document.getElementById("tel");
+    celular.addEventListener('keyup', (e) => {
+        let valorInput = e.target.value;
 
+        celular.value = valorInput
+            // Eliminamos espacios en blanco
+            .replace(/\s/g, '')
+            // Eliminar las letras
+            .replace(/\D/g, '')
+            // Ponemos espacio cada cuatro numeros
+            .replace(/([0-9]{3})/g, '$1 ')
+            // Elimina el ultimo espaciado
+            .trim();
+
+        // numeroTarjeta.textContent = valorInput;
+    });
 });
